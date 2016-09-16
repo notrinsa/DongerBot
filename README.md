edd12ed4fef1c1f5b7fedf1b1ee7138404fb0307
Benjamin Bloas <subtlefrequency@gmail.com>
2016-09-16 14:49:37 +0200

    Version 2 - Changelog dans le readme

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 DongerBot 0.1
 ============
 
-Codé par rinsa <subtlefrequency@gmail.com> pour #reddit-fr, avec comme base modifiée LogBot créé par Chris Oliver <chris@excid3.com>
+Codé par rinsa <me@ben.ovh> pour #reddit-fr / /r/France
 
 Utilisation
 -----
 
-Le bot nécessite Python2 pour fonctionner.
-Pour lancer le bot, il suffit de lancer le script comme n'importe quel autre script python
+Le bot nécessite Python2 pour fonctionner.  
+Pour lancer le bot, il suffit d'exécuter le script comme n'importe quel autre script python
 
     python dongerbot.py
-
 
 Commandes disponibles
 -----
 
-Les différentes actions ou insultes sont contenues dans leur fichier respectif (actions.json et insultes.json).
-Les arguments commencent par le signe dollar '$'. Pour que l'argument soit pris en compte, celui-ci doit être le pseudo d'un utilisateur connecté.
-Il existe également une commande à envoyer en privé au bot pour que celui-ci répète la phrase passée en argument, exemple :
-"!repeat XnS est vraiment stupide comme gars"
+Les différentes actions sont contenues dans le fichier **liste.json**  
+L'argument utilisé et pris en compte est %s. Pour que celui-ci soit pris en compte, celui-ci doit être le pseudo d'un utilisateur connecté. S'il n'en est pas un, le pseudo par défaut prend le dessus s'il existe dans le paramètre de l'action.  
+Il existe également une commande à envoyer en privé au DongerBot pour que celui-ci répète sur le canal public la phrase passée en argument, exemple :  
+
+    "!repeat XnS est vraiment stupide comme gars"
 
 
 Configuration
@@ -31,3 +31,16 @@ Archivage
 -----
 
 Les commandes exécutées en public et en privé sont repertoriées dans le dossier logs/ à la racine du dossier du Bot.
+
+Changelog
+-----
+
+##### Version 2 - 20160916
+- Refonte de la liste des actions
+- Possibilité de blacklister un utilisateur par le bot (!blacklist, !unignore)
+- Nouvelles commandes spéciales : !random, !friend, !who
+- Suppression de la liste communiquée en privée
+- Ajout d'un exemple de page listant la totalité des actions
+
+##### Version 1 - 20160721
+- Première version
--- dongerbot.py
+++ dongerbot.py
@@ -2,18 +2,22 @@
 # coding: utf-8
 
 __author__ = "rinsa"
-__version__ = "0.1"
-__date__ = "12/07/2016"
+__version__ = "1.0"
+__date__ = "2016-09-16"
 __copyright__ = "Copyright (c) rinsa"
 __license__ = "GPL2"
 
 
 import cgi
 import os
+from os import path
 import ftplib
 import sys
+reload(sys)  
+sys.setdefaultencoding('utf8')
 import itertools
 import time
+import random
 from time import strftime, localtime
 try:
     from datetime import datetime
@@ -26,7 +30,7 @@ except:
     import md5
 
 from ircbot import SingleServerIRCBot
-from irclib import nm_to_n
+from irclib import nm_to_n, nm_to_h
 
 import re
 import shlex
@@ -37,30 +41,36 @@ import random
 from pprint import pprint
 
 # IRC Server Configuration
-SERVER = ""
+SERVER = "0.0.0.0"
 PORT = 6667
 SERVER_PASS = None
-__channel__ = ""
-__nick__ = "dongerz"
-__nickpass__ = ""
-__logpath__ = "/path/to/the/log/folder"
-__super_secret_command__ = "!repeat"
-
+CHANNEL = "#!"
+NICK = "donger"
+NICKPASS = ""
 DEFAULT_TIMEZONE = 'GMT+2'
+# File Info
+LOCATION_PATH = "/path/to/where/the/bot/is/located"
+LOGPATH = LOCATION_PATH + "logs/"
+BLACKLIST = LOCATION_PATH + "blacklist.txt"
+ACTIONS_FILE = LOCATION_PATH + "liste.json"
+#
+SS_COMMAND = "!repeat"
+HOSTNAME_OP = "maisonclo.se" # a static mask is preferred
+# MY OLD FRIEND
+CURRENT_FRIEND = "donger"
 
 ### DongerBot class
+
 class DongerBot(SingleServerIRCBot):
     def __init__(self, server, port, server_pass=None, channels=[],
-                 nick="timber", nick_pass=None):
+                 nick="donger", nick_pass=None):
         SingleServerIRCBot.__init__(self,
                                     [(server, port, server_pass)],
                                     nick,
                                     nick)
 
-        self.chans = __channel__
-        self.superspam = 0
-        self.lastmessage = 0
-        self.nick_pass = nick_pass
+        self.chans = CHANNEL
+        self.nick_pass = NICKPASS
 
         print "DongerBot %s" % __version__
         print "Connecting to %s:%i..." % (server, port)
@@ -75,163 +85,230 @@ class DongerBot(SingleServerIRCBot):
             c.privmsg("nickserv", "identify %s" % self.nick_pass)
             c.join(self.chans)
 
+    # si le nom d'utilisateur est déjà utilisé, rajout d'underscore
     def on_nicknameinuse(self, c, e):
         """Nickname in use"""
         c.nick(c.get_nickname() + "_")
-
+    
+    # Traitement des messages privés
     def on_privmsg(self, connection, infos):
-        chan = self.channels[__channel__]
+        chan = self.channels[CHANNEL]
         users = chan.users()
         message = infos.arguments()[0].rstrip()
         if re.search("^!\w+", message):
             commande = re.findall("^!\w+", message)
-            if commande[0] == __super_secret_command__:
-                self.send_pub_msg(connection, message.replace(__super_secret_command__ + " ", ""))
+            if commande[0] == SS_COMMAND:
+                self.send_pub_msg(connection, message.replace(SS_COMMAND + " ", ""))
                 message = "[" + strftime("%Y-%m-%d %H:%M:%S", localtime()) + "]   " + nm_to_n(infos.source()) + " : " + message.replace("!repeat ", "") + "\n"
-                self.write_log_file(message, __logpath__ + "log_repeat_" + strftime("%Y-%m-%d", localtime()) + ".txt")
+                self.write_file(message, LOGPATH + "log_repeat_" + strftime("%Y-%m-%d", localtime()) + ".txt")
                 return
-        self.send_donger(connection, infos, __channel__, users)
-
-    def send_pub_msg(self, connection, message):
-        connection.privmsg(__channel__, message)
-
-    def write_log_file(self, message, file):
-        fo = open(file, "a")
-        fo.write(message)
-        fo.close
+        self.send_donger(connection, infos, CHANNEL, users)
     
+    # Traitement des messages publics
     def on_pubmsg(self, connection, infos):
-        auteur = nm_to_n(infos.source())
         canal = (infos.target(), self.channels[infos.target()])
         users = canal[1].users()
         chan = canal[0]
-        self.send_donger(connection, infos, chan, users)
-        
-    def format_message(self, actions, commande, message, users):
+        message = infos.arguments()[0].rstrip()
+        if message.startswith("!") and len(message) > 1:
+            self.send_donger(connection, infos, chan, users)
+    
+    # envoie un message dans le channel
+    def send_pub_msg(self, connection, message):
+        connection.privmsg(CHANNEL, message)
+
+    # fonction d'écriture de fichier
+    def write_file(self, message, file):
+        fo = open(file, "a")
+        fo.write(message)
+        fo.close
+
+    # rajoute un utilisateur / mask dans le fichier de blacklist
+    def ignore_user(self, user, mask=""):
+        ignored = self.get_ignored_users()
+        # if (user not in ignored and mask not in ignored):
+            # self.write_file(user + "\n" + mask + "\n", __blacklist_file__)
+        if (user not in ignored):
+            self.write_file(user + "\n", BLACKLIST)
+
+    # retire une ligne du fichier blacklist
+    def unignore(self, user, mask=""):
+        f = open(BLACKLIST, "r")
+        lines = f.readlines()
+        f.close()
+        f = open(BLACKLIST, "w")
+        for line in lines:
+            # if (line != user + "\n" or line != mask + "\n"):
+            if (line != user + "\n"):
+                f.write(line)
+        f.close
+    
+    # récupère chaque utilisateur blacklisté
+    def get_ignored_users(self):
+        if os.path.exists(BLACKLIST) is False:
+            return []
+        with open(BLACKLIST) as fo:
+            ignored_users = fo.read().splitlines()
+            return ignored_users
+    
+    # formate le message de sortie
+    def format_message(self, donger, message, users, auteur):
         # on split les arguments
         args = re.split('\s', message)
         # on supprime le nom de la commande
-        del args[0]
-        # on recup' le nombre d'arguments
-        taille = len(args)
-        newstr = actions[commande[0]]
-        nbmin = len(re.split("\$\w+", newstr)) - 1
-        # if (nbmin == taille and time.time() - self.lastmessage > 1.5): # pour eviter le spam
-        if nbmin == taille:
-            # on remplace les $x par les arguments
-            for i in range(0, taille):
-                # on verifie si le pseudo existe bien pour eviter n'imp
-                if args[i] not in users:
-                    return False
-                # commandes qui changent le pseudo en majuscule
-                if commande[0] in ["!aura"]:
-                    args[i] = args[i].upper()
-                newstr = newstr.replace("$" + str(i + 1), args[i])
-            # on envoie le message
-            return newstr
+        if len(args) > 1:
+            message = message.replace(args[0], "", 1)
+            message = message.strip()
         else:
-            return False
-    
-    def create_list(self, actions):
-            # declaration du tableau pour toute les commandes
-            listearray = []
-            listearray.append("Commandes disponibles : ")
-            # on parcourt les cles du dictionnaire d'actions
-            for key, value in actions.iteritems():
-                # affichage de la commande mise en forme
-                listestr = ""
-                listestr += key
-                diffargs = re.findall("\$\w+", value)
-                # si la commande possede des arguments, on rajoute pseudo + numero de l'argument e la suite
-                for i in range(0, len(diffargs)):
-                    listestr += " {pseudo " + str(i + 1) + "}"
-                listestr += ", "
-                listearray.append(listestr)
-            list2send = []
-            jump = 0
-            maximum = 10 # nombre maximum de commandes par ligne
-            for k in range(0, len(listearray)):
-                if jump == k:
-                    max = len(listearray) - k
-                    superstr = ""
-                    if max < maximum:
-                        for l in range (k, len(listearray)):
-                            # list2send.append(listearray[l].encode('utf-8', 'ignore'))
-                            superstr += listearray[l].encode('utf-8', 'ignore')
-                        list2send.append(superstr[:-2])
+            message = None
+
+        # On balance chacune des infos si elles existent dans des variables
+        donger_msg = donger['action']
+        if 'args' in donger:
+            donger_args = donger['args']
+            donger_caps = donger['caps']
+            if 'default' in donger:
+                donger_default = donger['default']
+            else:
+                donger_default = None
+        else:
+            donger_args = 0
+            
+        # donger_args à 1 = pseudos
+        if donger_args == 1:
+            if message is not None:
+                # si l'argument n'est pas vide et contient un utilisateur, sinon on dégage
+                if message.upper() in (user.upper() for user in users):
+                    if donger_caps:
+                        send_msg = donger_msg % message.upper()
                     else:
-                        for l in range (k, k + maximum):
-                            # list2send.append(listearray[l].encode('utf-8', 'ignore'))
-                            superstr += listearray[l].encode('utf-8', 'ignore')
-                        list2send.append(superstr)
-                    jump = k + maximum
-            self.lastmessage = time.time()
-            return list2send
+                        send_msg = donger_msg % message
+                else:
+                    send_msg = False
+            elif message is None:
+                # si l'argument est vide et si le default n'est pas vide, sinon on dégage
+                if donger_default is not None:
+                    if donger_caps:
+                        send_msg = donger_msg % donger_default.upper()
+                    else:
+                        send_msg = donger_msg % donger_default
+                else:
+                    send_msg = False
+            else:
+                return
+        # donger_args à 2 = texte
+        elif donger_args == 2:
+            # si y a pas de texte, on dégage
+            if message is not None:
+                send_msg = donger_msg % message
+            else:
+                send_msg = False
+        # donger_args à 3 = spécial
+        elif donger_args == 3:
+            # si la commande est !who, on dit qui est l'ami actuel
+            if donger['commande'] == '!who':
+                send_msg = donger_msg.format(CURRENT_FRIEND)
+            elif donger['commande'] == '!friend':
+                # si l'ami actuel est différent de l'auteur, on change celui-ci et donger est content
+                if CURRENT_FRIEND != auteur:
+                    send_msg = donger_msg.format(CURRENT_FRIEND, auteur.upper())
+                    self.change_friend(auteur)
+                # sinon on renvoie un !who, mais comme je suis trop nul j'ai écrit en dur
+                else:
+                    send_msg = "⭐️ {0} ⭐️ est mon ❤️ meilleur ami ❤️".format(CURRENT_FRIEND)
+        # donger_args à 0 / None / null
+        else:
+            send_msg = donger_msg
             
-            
-    def send_donger(self, connection, infos, target, users):
+        # return
+        return send_msg
         
-        auteur = nm_to_n(infos.source())
+    # oui je suis ton ami
+    def change_friend(self, new):
+        global CURRENT_FRIEND
+        CURRENT_FRIEND = new
+
+    # Récupère le fichier JSON
+    def load_file(self):
+        with open(ACTIONS_FILE) as data_file:    
+            actions = json.load(data_file)   
+        full_list = {}
+        # On aplatit le fichier JSON en enlevant les catégories
+        for key in actions:
+            for dkey in actions[key]:
+                donger = {}
+                donger['action'] = actions[key][dkey]['action'] 
+                donger['commande'] = actions[key][dkey]['commande'] 
+                if 'args' in actions[key][dkey]:
+                    donger['args'] = actions[key][dkey]['args']
+                    donger['caps'] = actions[key][dkey]['caps']
+                    if 'default' in actions[key][dkey]:
+                        donger['default'] = actions[key][dkey]['default']
+                full_list[actions[key][dkey]['commande'] ] = donger
+        return full_list
+    
+    # Pas de fonction native à la lib qui permet de récupérer le nom@mask :|
+    def nm_to_fn(self, auteur):
+        return auteur.split("!")[1]
+    
+    # On envoie le donger
+    def send_donger(self, connection, infos, target, users):
+        auteur_full = infos.source()
+        auteur_fn = self.nm_to_fn(auteur_full)
+        auteur_mask = nm_to_h(auteur_full)
+        auteur = nm_to_n(auteur_full)
         canal = (target, connection)
         message = infos.arguments()[0].rstrip()
-        
-        # trigger d'insultes si on prononce le pseudo du bot
-        if __nick__ in message:
-            with open('/home/rinsa/pastabot/insultes.json') as insultes_file:    
-                insultes = json.load(insultes_file)
-            # insulte R A N D O M
-            insulte = random.choice(insultes.items())
-            # si l'insulte a un argument, on le remplace
-            insulte = insulte[1].replace("$1", auteur)
-            # timer 1 seconde pour simuler un comportement humain
-            time.sleep(1)
-            connection.privmsg(canal[0], insulte.encode('utf-8', 'ignore'))
+        if auteur_fn in self.get_ignored_users():
             return
-            
+
         # on ouvre le fichier d'actions
-        with open('/home/rinsa/pastabot/actions.json') as data_file:    
-            actions = json.load(data_file)
-            
-        # si le message envoye sur le chan commence par ! / on fait pas de verification si la commande existe car il y a des commandes prises en compte et non listee
-        if re.search("^!\w+", message):
-            # on recupere le nom de la commande
-            commande = re.findall("^!\w+", message)
-            
-            # la liste !
-            if commande[0] == "!liste":
-                # on evite le spam, envoi possible toutes les 5 minutes ///// 20 secondes maintenant car envoi de la liste via mp
-                if time.time() - self.lastmessage < 20:
-                    if time.time() - self.superspam < 9:
-                        self.superspam = time.time()
-                        return
-                    elif time.time() - self.superspam > 1.5:
-                        connection.privmsg(auteur, "ça va le spam ou quoi ?")
-                        self.superspam = time.time()
-                        return
-                    else:
-                        return
-                # on créé le tableau des commandes dispos
-                liste = self.create_list(actions)
-                for li in range(0, len(liste)):
-                    connection.privmsg(auteur, liste[li])
-                    # on envoie la commandes toutes les secondes et demi pour éviter un mass flood
-                    time.sleep(1.5)
-                    
-            # donc on verifie si la commande existe bien dans la liste des actions
-            if commande[0] in actions:
-                sendmsg = self.format_message(actions, commande, message, users)
-                if sendmsg is not False:
-                    # on envoie le message dans le canal spécifié
-                    connection.privmsg(canal[0], sendmsg.encode('utf-8', 'ignore'))
-                    # on créé le message de log au format [DATE] pseudo : commande 
-                    log_message = "[" + strftime("%Y-%m-%d %H:%M:%S", localtime()) + "]   " + nm_to_n(auteur) + " : " + message + "\n"
-                    # on écrit le message dans le fichier de log
-                    self.write_log_file(log_message, __logpath__ + "log_public_" + strftime("%Y-%m-%d", localtime()) + ".txt")
-                    
+        liste = self.load_file()
+
+        # on récupère la commande
+        pattern = re.compile("^!\w+")
+        if pattern.match(message):
+            commande = re.findall("^!\w+", message)[0]
+        else:
+            return
+        
+        # commande dans la liste ?
+        if commande in liste:
+            sendmsg = self.format_message(liste[commande], message, users, auteur)
+            if sendmsg is False:
+                return
+            else:
+                self.send_pub_msg(connection, sendmsg.encode('utf-8', 'ignore'))
+        # Commande !blacklist !unignore
+        elif commande in ['!blacklist', '!unignore'] and auteur_mask == 'maisonclo.se':
+            args = re.split('\s', message)
+            if len(args) == 2:
+                message = message.replace(args[0], "", 1)
+                message = message.strip()
+            else:
+                return
+            if commande == '!blacklist':
+                self.ignore_user(message)    
+            if commande == '!unignore':
+                self.unignore(message)
+        # Commande !liste
+        elif commande == '!liste':
+            self.send_pub_msg(connection, "La liste des commandes est disponible sur www.redditairfrance.fr")
+        # Envoie une commande au hasard avec un utilisateur au hasard
+        elif commande == '!random':
+            r_commande = liste[random.choice(liste.keys())]
+            if 'args' in r_commande and r_commande['args'] == 2:
+                return
+            r_user = random.choice(users)
+            r_message = r_commande['commande'] + " " + r_user
+            sendmsg = self.format_message(r_commande, r_message, users, auteur)
+            if sendmsg is False:
+                return
+            self.send_pub_msg(connection, sendmsg.encode('utf-8', 'ignore'))
 
 def main():
     # Start the bot
-    bot = DongerBot(SERVER, PORT, SERVER_PASS, __channel__, __nick__, __nickpass__)
+    bot = DongerBot(SERVER, PORT, SERVER_PASS, CHANNEL, NICK, NICKPASS)
     try:
         bot.start()
     except KeyboardInterrupt:
